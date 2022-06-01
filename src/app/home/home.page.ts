1b429402bb25693e1f095f7754e1957c2530018e
Developer <developer@pop-os.localdomain>
2022-06-01 17:18:14 +0300

    add(android) adding test with android and fixing the multiple tooltip po up on the mobile

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,10 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-
 interface SentenceData {
-  id: string,
-  value: string,
-  selected: boolean
+  id: string;
+  value: string;
+  selected: boolean;
 }
 @Component({
   selector: 'app-home',
@@ -13,15 +12,25 @@ interface SentenceData {
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  dataText = "";
+  dataText = '';
   sentences: SentenceData[] = [];
   wordsCount = 0;
   followTooltipVisible = false;
-  
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.sentences = this
+      .processText(`The zeitgeist contends that those seashores are nothing more than punishments.
+       A board is a blowgun from the right perspective. The octagon of a condition becomes a wayless newsprint. 
+       The literature would have us believe that a farand range is not but a ski. 
+       A wind can hardly be considered a plumbless rugby without also being a norwegian. 
+       We know that the unfree selection reveals itself as a jellied salt to those who look. 
+       Their burma was, in this moment, a longish mice. 
+       This is not to discredit the idea that a spicy carnation is a female of the mind. 
+       Before policemen, baths were only josephs. The literature would have us believe that a towy slash is not but a nose. 
+       Some assert that a gemini can hardly be considered a playful ladybug without also being a subway.
+        An ignored authorization without gasolines is truly a captain of squashy armies.
+       Few can name a tasseled sword that isn't a becalmed furniture. Recent controversy a`);
     const headers = new HttpHeaders().set(
       'Content-Type',
       'text/plain; charset=utf-8'
@@ -36,7 +45,7 @@ export class HomePage {
           // hideloader();
         }
         this.dataText = response;
-        this.sentences = this.processText(this.dataText)
+        this.sentences = this.processText(this.dataText);
       });
     function hideloader() {
       document.getElementById('loading').style.display = 'none';
@@ -44,42 +53,39 @@ export class HomePage {
   }
 
   processText(text) {
-    const sentences = text.split('.')
-      .map(sentence => `${sentence}.`)
+    const sentences = text
+      .split('.')
+      .map((sentence) => `${sentence}.`)
       .map((sentence, index) => {
-        return { id: index, value: sentence, selected: false }
-      })
-    return sentences
+        return { id: index, value: sentence, selected: false };
+      });
+    return sentences;
   }
 
   onSelectSentences = (id) => {
-    const updatedSSentencesSelection = this.sentences.map(sentence => {
+    const updatedSSentencesSelection = this.sentences.map((sentence) => {
       if (sentence.id === id) {
-        const updateSelect = { ...sentence, selected: !sentence.selected }
-        return updateSelect
+        const updateSelect = { ...sentence, selected: !sentence.selected };
+        return updateSelect;
       } else {
-        return sentence
+        return sentence;
       }
-    }
-    )
-    this.sentences = updatedSSentencesSelection
-  }
+    });
+    this.sentences = updatedSSentencesSelection;
+  };
   handlesentenceHover(event: any) {
-    const sentenceText = event.target.innerText
-    const countWords = sentenceText.trim().split(' ').length
-    this.wordsCount = countWords
-
+    const sentenceText = event.target.innerText;
+    const countWords = sentenceText.trim().split(' ').length;
+    this.wordsCount = countWords;
   }
   handleSentenceMouseEnter() {
-   this.followTooltipVisible = true
+    this.followTooltipVisible = true;
   }
   handleSentenceMouseLeave() {
-    this.followTooltipVisible = false 
+    this.followTooltipVisible = false;
   }
 
-  ionViewDidEnter(){
-   return this.ngOnInit()
+  ionViewDidEnter() {
+    return this.ngOnInit();
+  }
 }
-}
-
-
--- src/app/tooltip/tooltip.directive.ts
+++ src/app/tooltip/tooltip.directive.ts
@@ -1,59 +1,65 @@
 import {
-    Directive,
-    ElementRef,
-    HostListener,
-    Input,
-    OnDestroy,
-  } from '@angular/core';
-  @Directive({
-    selector: '[tooltip]',
-  })
-  export class TooltipDirective implements OnDestroy {
-    @Input() tooltip = ''; // The text for the tooltip to display
-    @Input() delay? = 400; // Optional delay input, in ms
-    @Input() wordsNumber = '';
-    private myPopup;
-    private timer;
-    constructor(private el: ElementRef) {}
-    ngOnDestroy(): void {
-      if (this.myPopup) {
-        this.myPopup.remove();
-      }
-    }
-    @HostListener('mouseenter', ['$event']) onMouseEnter(e) {
-      this.timer = setTimeout(() => {
-        let x = e.clientX + 10; //x position within the element.
-        let y = e.clientY + 10;
-        this.createTooltipPopup(x, y);
-      }, this.delay);
-    }
-    @HostListener('mouseleave') onMouseLeave() {
-      if (this.timer) clearTimeout(this.timer);
-      if (this.myPopup) {
-        this.myPopup.remove();
-      }
-    }
-    @HostListener('document: mousemove', ['$event']) onMouseMove(e) {
-      const pageX = e.pageX;
-      const pageY = e.pageY;
-      const x = e.clientX + 10;
-      const y = e.clientY + 10;
-      if (this.myPopup) {
-        this.myPopup.style.left = `${x}px`;
-        this.myPopup.style.top = `${y}px`;
-      }
-    }
-    private createTooltipPopup(x: number, y: number) {
-      let popup = document.createElement('div');
-      popup.innerHTML = this.tooltip + this.wordsNumber;
-      popup.setAttribute('class', 'tooltipContainer');
-      popup.style.top = y.toString() + 'px';
-      popup.style.left = x.toString() + 'px';
-      popup.style.position = 'absolute';
-      document.body.appendChild(popup);
-      this.myPopup = popup;
-      setTimeout(() => {
-        if (this.myPopup) this.myPopup.remove();
-      }, 156000); // Remove tooltip after 5 seconds
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  OnDestroy,
+} from '@angular/core';
+@Directive({
+  selector: '[tooltip]',
+})
+export class TooltipDirective implements OnDestroy {
+  @Input() tooltip = ''; // The text for the tooltip to display
+  @Input() delay? = 400; // Optional delay input, in ms
+  @Input() wordsNumber = '';
+  private myPopup;
+  private timer;
+  constructor(private el: ElementRef) {}
+  ngOnDestroy(): void {
+    if (this.myPopup) {
+      this.myPopup.remove();
     }
   }
+  @HostListener('mouseenter', ['$event']) onMouseEnter(e) {
+    this.timer = setTimeout(() => {
+      let x = e.clientX + 10; //x position within the element.
+      let y = e.clientY + 10;
+      this.createTooltipPopup(x, y);
+    }, this.delay);
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    if (this.timer) clearTimeout(this.timer);
+    if (this.myPopup) {
+      this.myPopup.remove();
+    }
+  }
+  @HostListener('document: mousemove', ['$event']) onMouseMove(e) {
+    const pageX = e.pageX;
+    const pageY = e.pageY;
+    const x = e.clientX + 10;
+    const y = e.clientY + 10;
+    if (this.myPopup) {
+      this.myPopup.style.left = `${x}px`;
+      this.myPopup.style.top = `${y}px`;
+    }
+  }
+  private createTooltipPopup(x: number, y: number) {
+    if (this.myPopup) this.myPopup.remove();
+    let popup = document.createElement('div');
+    let existingTooltip = document
+      .querySelectorAll('#tooltip-id')
+      .forEach((el) => el.remove());
+    popup.innerHTML = this.tooltip + this.wordsNumber;
+    popup.setAttribute('class', 'tooltipContainer');
+    popup.setAttribute('id', 'tooltip-id');
+    console.log(popup, 'popup');
+    popup.style.top = y.toString() + 'px';
+    popup.style.left = x.toString() + 'px';
+    popup.style.position = 'absolute';
+    document.body.appendChild(popup);
+    this.myPopup = popup;
+    // setTimeout(() => {
+    //   if (this.myPopup) this.myPopup.remove();
+    // },2500); // Remove tooltip after 5 seconds
+  }
+}
